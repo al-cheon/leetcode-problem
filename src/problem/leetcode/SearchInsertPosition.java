ca838a9e474b4cd3c69228b7d8866660642b0f29
Jason <al.cheon03@gmail.com>
2023-01-04 13:36:08 +0900

    70. Climbing Stairs

--- src/problem/leetcode/SearchInsertPosition.java
+++ src/problem/leetcode/SearchInsertPosition.java
@@ -3,14 +3,14 @@ package problem.leetcode;
 public class SearchInsertPosition {
     public static void main(String[] args) {
         int[] nums = {1,3,5,6};
-        int target = 7;
+        int target = 2;
 
-        System.out.println(searchInsert(nums, target)); // 그냥 일반 탐색방법
+//        System.out.println(searchInsert1(nums, target)); // 그냥 일반 탐색방법
         System.out.println(searchInsert2(nums, target)); // 피벗을 활용한 탐색방법 O(log n) runtime complexity.
 
     }
 
-    public static int searchInsert(int[] nums, int target) {
+    public static int searchInsert1(int[] nums, int target) {
         int result = 0;
         boolean existed = false;
 
@@ -29,6 +29,7 @@ public class SearchInsertPosition {
 
     public static int searchInsert2(int[] nums, int target) {
         int pivot, left = 0, right = nums.length - 1;
+
         while (left <= right) {
             pivot = left + (right - left) / 2;
             if (nums[pivot] == target) return pivot;
